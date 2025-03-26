05db884c486c89ecc95008fd0b2e8bd495381a92
SooinDev <alwayswithsound@gmail.com>
2025-03-27 05:24:45 +0900

    fix: testUserService

--- src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
+++ src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
+    // UserService
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
