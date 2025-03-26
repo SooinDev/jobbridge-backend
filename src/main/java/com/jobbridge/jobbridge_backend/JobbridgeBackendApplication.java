6dcb9f79413104c20bb1f84a892dae9b5938b925
SooinDev <alwayswithsound@gmail.com>
2025-03-26 18:10:51 +0900

    test

--- src/main/java/com/jobbridge/jobbridge_backend/JobbridgeBackendApplication.java
+++ src/main/java/com/jobbridge/jobbridge_backend/JobbridgeBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobbridgeBackendApplication {
-	// ttttt
+	// ssss
 	public static void main(String[] args) {
 		SpringApplication.run(JobbridgeBackendApplication.class, args);
 	}
