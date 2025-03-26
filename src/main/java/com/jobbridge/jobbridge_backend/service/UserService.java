a825668e6f1a701c00b4e7ba03243594037493b0
SooinDev <alwayswithsound@gmail.com>
2025-03-27 05:55:02 +0900

    Save changes to UserService.java

--- src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
+++ src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
+    // application.properties 삭제
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
