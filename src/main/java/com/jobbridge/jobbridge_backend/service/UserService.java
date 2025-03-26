7e62dee2d1ee4e9dbc490a02ab522395a89db9c9
SooinDev <alwayswithsound@gmail.com>
2025-03-27 05:35:14 +0900

    fix: ddd

--- src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
+++ src/main/java/com/jobbridge/jobbridge_backend/service/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
   
-    // UserService
+    // dddddddddd
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
