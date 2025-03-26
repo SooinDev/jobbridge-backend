9977379bb46dc24a23bd4b697946add255fc5a9b
SooinDev <alwayswithsound@gmail.com>
2025-03-26 17:56:24 +0900

    init: spring boot backend

---
+++ src/main/java/com/jobbridge/jobbridge_backend/JobbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobbridge.jobbridge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbridgeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobbridge/jobbridge_backend/JobbridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobbridge.jobbridge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
